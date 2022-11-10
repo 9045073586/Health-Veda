cd9e465a2f752125ed098ae94298ca36d3b21a7e
rahulchaudhary777 <ch.rahuljaat777@gmail.com>
2022-11-10 23:43:19 +0530

    ...

--- popup.js
+++ popup.js
@@ -14,13 +14,33 @@ function closePopup() {
 var thanks = document.getElementsByClassName("thanks")[0];
 
 function hide() {
-  thanks.style.transition="0.4s linear";
+  thanks.style.transition="0.2s linear";
   thanks.style.transform = "translate(50%, 50%)";
   thanks.classList.remove("thanks-visible");
   
 }
 
+// prevent default behaviour of form
+document.getElementById("form").addEventListener("click", (event) => {
+  event.preventDefault();
+})
+
 function thanksVisible() {
   popup.classList.remove("open-popup");
   thanks.classList.add("thanks-visible");
 }
+
+setInterval(function time() {
+  var d = new Date();
+  var hours = 24 - d.getHours();
+  var min = 60 - d.getMinutes();
+  if ((min + "").length == 1) {
+    min = "0" + min;
+  }
+  var sec = 60 - d.getSeconds();
+  if ((sec + "").length == 1) {
+    sec = "0" + sec;
+  }
+  document.getElementById("thanksPara").innerHTML =
+    `our team call you back within ${hours}:${min}:${sec} hours.`;
+}, 1000);
